ddfce2a4ce869bb29e9672bab8bf0daa3d591777
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-25 23:10:36 -0700

    Landing page milestone 2.

--- index.php
+++ index.php
@@ -1,10 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="icon" type="iamge/png" href="http://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Linea_K_(Logo_Metro_Medellin).svg/2000px-Linea_K_(Logo_Metro_Medellin).svg.png">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="css/animate.css">
+
 </head>
 <body>
 
+
+
+<style type="text/css">
+	body{
+		background-color: blue;
+	}
+
+
+
+</style>
+<div id="title" class="slide header">
+  <h1 class="button animated zoomInDown">Landing page</h1>
+</div>
+
+<div id="slide1" class="slide">
+  <div class="title">
+    <h1>To-Do List</h1>
+    <p><a href="http://localhost/Kenneth/index.php"> To-Do List</a></p>
+  </div>
+</div>
+
+<div id="slide2" class="slide">
+  <div class="title">
+    <h1>Slide 2</h1>
+    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
+  </div> 
+</div>
+
+<div id="slide3" class="slide">
+  <div class="title">
+    <h1>Slide 3</h1>
+    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
+  </div>
+</div>
+
+<div id="slide4" class="slide header">
+    <h1>The End</h1>
+</div>
 </body>
 </html>
