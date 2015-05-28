ebc6c2b2175656efc5f39023e704a2fef3a9f977
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-28 11:37:24 -0700

    Landing page commit.

--- index.php
+++ index.php
@@ -6,26 +6,78 @@
 	<link rel="icon" type="iamge/png" href="http://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Linea_K_(Logo_Metro_Medellin).svg/2000px-Linea_K_(Logo_Metro_Medellin).svg.png">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+<header>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">
+          <i class="fa fa-diamond"></i><br>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-bolt"></i><br>
+          Info
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-briefcase"></i><br>
+          Work
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-heartbeat"></i><br>
+          Contact
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-sun-o"></i><br>
+          Gallery
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
 
 </head>
 <body>
 
 
 
+<div id="title" class="slide header">
 <style type="text/css">
-	body{
-		background-color: blue;
-	}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
 
-
+li {
+    display: inline;
+}
 
 </style>
-<div id="title" class="slide header">
+</head>
+<body>
+
+<ul>
+
+</ul>
+
+</body>
+
   <h1 class="button animated zoomInDown">Landing page</h1>
 </div>
 
 <div id="slide1" class="slide">
   <div class="title">
+  
     <h1>To-Do List</h1>
     <p><a href="http://localhost/Kenneth/index.php"> To-Do List</a></p>
   </div>
@@ -40,8 +92,9 @@
 
 <div id="slide3" class="slide">
   <div class="title">
+
     <h1>Slide 3</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
+    <p></p>
   </div>
 </div>
 
